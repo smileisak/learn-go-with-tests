379edffdb20e788e8931d9981848ec4bd81e88c5
Ismail KABOUBI <ikaboubi@gmail.com>
2020-05-06 21:11:43 +0200

    TDD: add spanish lang

--- hello/hello.go
+++ hello/hello.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
 
 // Hello function to return a string to say hello
-func Hello(name string) string {
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+	if language == "Spanish" {
+		return spanishHelloPrefix + name
+	}
 	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", "spanish"))
 }
