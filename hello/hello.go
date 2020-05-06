eed71b2852f5b339746dc698352a39f78f1021fc
Ismail KABOUBI <ikaboubi@gmail.com>
2020-05-06 21:14:05 +0200

    Finish If statement

--- hello/hello.go
+++ hello/hello.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
 const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
 
 // Hello function to return a string to say hello
 func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	if language == "Spanish" {
+	if language == spanish {
 		return spanishHelloPrefix + name
+	}
+	if language == french {
+		return frenchHelloPrefix + name
 	}
 	return englishHelloPrefix + name
 }
--- hello/hello_test.go
+++ hello/hello_test.go
@@ -28,4 +28,9 @@ func TestHello(t *testing.T) {
 		want := "Hola, Elodie"
 		assertCorrectMessage(t, got, want)
 	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
 }
