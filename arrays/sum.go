f6cd23aa6fc031ab0415319521c999e3b6703e4a
Ismail KABOUBI <ikaboubi@gmail.com>
2020-05-08 14:06:29 +0200

    Finish Arrays chapter

--- arrays/sum.go
+++ arrays/sum.go
@@ -9,3 +9,34 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+// SumAll calculate the sum of slices and return the result in a slice
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// sums := make([]int, lengthOfNumbers)
+
+	// for i, numbers := range numbersToSum {
+	// 	sums[i] = Sum(numbers)
+	// }
+	// return sums
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return sums
+}
+
+// SumAllTails calculates the totals of the "tails" of each slice.
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tails := numbers[1:]
+			sums = append(sums, Sum(tails))
+		}
+
+	}
+	return sums
+}
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -12,4 +15,35 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d but given %d, %v.", got, want, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 3}, []int{4, 5, 6})
+	want := []int{6, 15}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v but want %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v want %v", got, want)
+		}
+	}
+
+	t.Run("make the sum of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		checkSums(t, got, want)
+	})
+	t.Run("make the sum of an empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+		checkSums(t, got, want)
+	})
+
 }
