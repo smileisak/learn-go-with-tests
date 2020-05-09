668cc9c15e3113ad8fcf45d3626dcece7da3cb02
Ismail KABOUBI <ikaboubi@gmail.com>
2020-05-09 19:43:07 +0200

    Still progressing on reflect

---
+++ reflexion/reflexion.go
@@ -0,0 +1,35 @@
+package reflexion
+
+import "reflect"
+
+func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	numberOfValues := 0
+	var getField func(int) reflect.Value
+
+	switch val.Kind() {
+	case reflect.String:
+		fn(val.String())
+	case reflect.Struct:
+		numberOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice:
+		numberOfValues = val.Len()
+		getField = val.Index
+	}
+
+	for i := 0; i < numberOfValues; i++ {
+		walk(getField(i).Interface(), fn)
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+	if val.Kind() == reflect.Ptr {
+		// You can't use NumField on a pointer Value,
+		// we need to extract the underlying value before we can do that by using Elem().
+		val = val.Elem()
+	}
+	return val
+}
