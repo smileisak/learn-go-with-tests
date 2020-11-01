06791b59a0198d19397146bcfaa0d69171ad23fd
Ismail KABOUBI <ikaboubi@gmail.com>
2020-11-01 18:26:29 +0100

    Fix superfluous response.WriteHeader

--- json/server.go
+++ json/server.go
@@ -42,8 +42,8 @@ func NewPlayerServer(store PlayerStore) *PlayerServer {
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("content-type", "application/json")
-	json.NewEncoder(w).Encode(p.store.GetLeague())
 	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(p.store.GetLeague())
 }
 
 func (p *PlayerServer) playersHandler(w http.ResponseWriter, r *http.Request) {
